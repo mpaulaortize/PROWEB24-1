1d0801ef2dd832c3045de979c0b65acf8b383b6a
Maria Paula <110471327+mpaulaortize@users.noreply.github.com>
2024-02-06 11:49:22 -0500

    Delete firstclass-app directory

--- firstclass-app/src/components/FooterApp/FooterApp.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export function FooterApp() {
-  return (
-    <footer>
-      <p>All copyright reserved P.</p>
-    </footer>
-  );
-}
--- firstclass-app/src/components/HeaderApp/HeaderApp.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export function HeaderApp() {
-  return (
-    <header>
-      <h3>Hello world</h3>
-    </header>
-  );
-}
